3adab5b33af05d03a9d8c4060865e9370cd96f62
fulldiver-drc <89520557+fulldiver-drc@users.noreply.github.com>
2022-03-04 14:57:20 +0800

    Update calculatorSelection.js

--- scripts/calculatorSelection.js
+++ scripts/calculatorSelection.js
@@ -10,40 +10,48 @@ function compareItems(item1, item2){
 
 var selectedItems = [];
 
-window.selectItem = function(index, select){
-  selectedItems[index].itemId = select.value;
+window.selectItem = function(select){
+  select.closest('[itemselect]').selectedItem.itemId = select.value;
 }
 
 function buildItemList(){
   var itemsClone = [...items];
+  if (itemsClone.length === 0){
+    for (var i=0; i<5; i++){
+      itemsClone.push({Id: i, Label: 'Test item ' + i});
+    }
+  }
   itemsClone.sort(compareItems);
   var options = `<option value='0'>Select an item</option>`;
   function addItemToList(item){
-    options += `<option value='${item.Id}'>${item.Label}</option>`; 
+    options += `<option value=${item.Id}>${item.Label}</option>`; 
   }
   itemsClone.forEach(addItemToList);
   return options;
 }
 
-var optionsText = buildItemList();
+const optionsText = buildItemList();
 
-function buildItemRow(index=0){
+function buildItemRow(){
   var nodeTemplate = document.createElement('div');
-  nodeTemplate.innerHTML = `<select required onchange='selectItem(${index}, this)'>`;
-  nodeTemplate.innerHTML += optionsText;
-  nodeTemplate.innerHTML += '</select>';
+  nodeTemplate.setAttribute('itemselect', '');
+  nodeTemplate.innerHTML = `<select required onchange='selectItem(this)'>${optionsText}</select>`;
   return nodeTemplate;
 }
 
 var itemSelection = document.getElementById('item-selection-main');
 
-window.addItem = function(index=0){
-  selectedItems.splice(index, 0, {itemId: 0, multiplier: 1});
-  itemSelection.insertBefore(buildItemRow(index), itemSelection.children[index]);
+window.addItem = function(){
+  var newNode = buildItemRow();
+  newNode.selectedItem = {itemId: 0, multiplier: 1};
+  
+  selectedItems.push(newNode.selectedItem);
+  itemSelection.appendChild(newNode);
 }
 
-window.removeItem = function(index){
-  if (index === null) return;
-  selectedItems.splice(index, 1);
+window.removeItem = function(button){
+  var itemDiv = button.closest('[itemselect]');
+  selectedItems.splice(indexOf(itemDiv.selectedItem), 1);
+  itemDiv.remove()
 }
 
