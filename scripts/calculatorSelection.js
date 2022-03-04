a69834541fa2589a542b5a426c64c00bf445873c
fulldiver-drc <89520557+fulldiver-drc@users.noreply.github.com>
2022-03-05 00:09:51 +0800

    Update calculatorSelection.js

--- scripts/calculatorSelection.js
+++ scripts/calculatorSelection.js
@@ -12,8 +12,9 @@ var selectedItems = [];
 
 window.selectItem = function(select, isMultiplier=false){
   var item = select.closest('[itemselect]').selectedItem;
-  if (isMultiplier)
-    item.multiplier = select.value;
+  if (isMultiplier){
+    item.multiplier = select.valueAsNumber;
+  }
   else
     item.itemId = select.value;
 }
@@ -40,7 +41,7 @@ function buildItemRow(){
   var nodeTemplate = document.createElement('div');
   nodeTemplate.setAttribute('itemselect', '');
   nodeTemplate.innerHTML = `<select required onchange='selectItem(this)' class='item-select'>${optionsText}</select>`;
-  nodeTemplate.innerHTML += `<input type='number' placeholder='Total level multiplier' value='1' class='item-multipy' onchange='selectItem(this, true)'/>`
+  nodeTemplate.innerHTML += `<input type='number' placeholder='Total level multiplier' value='1' class='item-multipy' onchange='selectItem(this, true)' min=1/>`
   return nodeTemplate;
 }
 
