d5808a12d4a4f044b551556ed7a806152a086ca4
fulldiver-drc <89520557+fulldiver-drc@users.noreply.github.com>
2022-03-06 23:13:24 +0800

    Update calculatorSelection.js

--- scripts/calculatorSelection.js
+++ scripts/calculatorSelection.js
@@ -1,12 +1,4 @@
-import {items} from './itemManagement.js';
-
-function compareItems(item1, item2){
-  if (item1.Label > item2.Label)
-     return 1;
-  if (item2.Label > item1.Label)
-    return -1;
-  return 0;
-}
+import {getItemList, sortSettings} from './itemManagement.js';
 
 var selectedItems = [];
 
@@ -21,19 +13,19 @@ window.selectItem = function(select, isMultiplier=false){
     item.itemId = parseInt(select.value);
 }
 
+function addItemToList(item){
+  this += `<option value=${item.Id}>${item.Label}</option>`;
+}
+
 function buildItemList(){
-  var itemsClone = [...items];
-  if (itemsClone.length === 0){
+  var items = getItemList('', sortSettings.label);
+  if (items.length === 0){
     for (var i=0; i<5; i++){
-      itemsClone.push({Id: i, Label: 'Test item ' + i});
+      items.push({Id: i, Label: 'Test item ' + i});
     }
   }
-  itemsClone.sort(compareItems);
   var options = `<option value=0>Select an item</option>`;
-  function addItemToList(item){
-    options += `<option value=${item.Id}>${item.Label}</option>`; 
-  }
-  itemsClone.forEach(addItemToList);
+  items.forEach(addItemToList, options);
   return options;
 }
 
