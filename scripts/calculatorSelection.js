fa5e60386d5a440b11063672458af3bdb54796f3
fulldiver-drc <89520557+fulldiver-drc@users.noreply.github.com>
2022-03-07 16:30:30 +0800

    Update calculatorSelection.js

--- scripts/calculatorSelection.js
+++ scripts/calculatorSelection.js
@@ -1,23 +1,5 @@
 import {getItemList, getItemDetails, sortSettings, generateSummary} from './itemManagement.js';
 
-var selectedItems = [];
-
-window.selectItem = function(select, isMultiplier=false){
-  var itemDiv = select.closest('[itemselect]');
-  var item = itemDiv.selectedItem;
-  var preview = itemDiv.querySelector('.item-quick-rate');
-  
-  if (isMultiplier){
-    if (select.valueAsNumber < 1)
-       select.value = 1;
-    item.multiplier = select.valueAsNumber;
-  }
-  else
-    item.itemId = parseInt(select.value);
-  
-  var itemDetail = getItemDetails(item.itemId);
-  preview.innerHTML = `Rate: ${(itemDetail.Base * item.multiplier).toFixed(2)} / min<br/>Building: ${itemDetail.Building}`
-}
 
 function buildItemList(){
   var items = getItemList('', sortSettings.label);
@@ -28,7 +10,6 @@ function buildItemList(){
   }
   return items.reduce((prevValue, item) => {return prevValue += `<option value=${item.Id}>${item.Label}</option>`}, '<option value=0>-Select an item-</option>');
 }
-
 const optionsText = buildItemList();
 
 function buildAddItemButton(){
@@ -43,34 +24,51 @@ function buildAddItemButton(){
 function buildItemRow(){
   var nodeTemplate = document.createElement('div');
   nodeTemplate.setAttribute('itemselect', '');
-  nodeTemplate.innerHTML = `<select required onchange='selectItem(this)' class='item-select'>${optionsText}</select>`;
-  nodeTemplate.innerHTML += `<input type='number' placeholder='Total level multiplier' value='1' class='item-multiply' onfocusout='selectItem(this, true)' min=1 />`;
-  nodeTemplate.innerHTML += `<a onclick='removeItem(this)' class='ico' removeitem>&#xE108</a><a onclick='addItem()' class='ico' additem>&#xE109</a>`;
+  nodeTemplate.innerHTML = `<select required onchange='recipeCalculator.selectItem(this)' class='item-select'>${optionsText}</select>`;
+  nodeTemplate.innerHTML += `<input type='number' placeholder='Total level multiplier' value='1' class='item-multiply' onfocusout='recipeCalculator.selectItem(this, true)' min=1 />`;
+  nodeTemplate.innerHTML += `<a onclick='recipeCalculator.removeItem(this)' class='ico' removeitem>&#xE108</a><a onclick='recipeCalculator.addItem()' class='ico' additem>&#xE109</a>`;
   nodeTemplate.innerHTML += `<div itemid=0 mult=0 class='item-quick-rate'>Rate: N/A<br/>Building: N/A</div>`; 
   return nodeTemplate;
 }
 
 var itemSelection = document.getElementById('item-selection-main');
+var selectedItems = [];
 
-window.addItem = function(){
-  var newNode = buildItemRow();
-  newNode.selectedItem = {itemId: 0, multiplier: 1};
-  document.querySelectorAll('[additem]').forEach(node => {node.remove()});
-  
-  selectedItems.push(newNode.selectedItem);
-  itemSelection.appendChild(newNode);
-}
+window.recipeCalculator = {
+  selectItem: function(select, isMultiplier=false){
+    var itemDiv = select.closest('[itemselect]');
+    var item = itemDiv.selectedItem;
+    var preview = itemDiv.querySelector('.item-quick-rate');
 
-window.removeItem = function(button){
-  var itemDiv = button.closest('[itemselect]');
-  selectedItems.splice(selectedItems.indexOf(itemDiv.selectedItem), 1);
-  
-  if (!itemDiv.nextElementSibling)
-    itemDiv.previousElementSibling.appendChild(buildAddItemButton());
-  itemDiv.remove()
-}
+    if (isMultiplier){
+      if (select.valueAsNumber < 1)
+         select.value = 1;
+      item.multiplier = select.valueAsNumber;
+    }
+    else
+      item.itemId = parseInt(select.value);
 
-window.logItems = function(){
-  console.log(generateSummary(selectedItems));
+    var itemDetail = getItemDetails(item.itemId);
+    preview.innerHTML = `Rate: ${(itemDetail.Base * item.multiplier).toFixed(2)} / min<br/>Building: ${itemDetail.Building}`
+  },
+  addItem: function(){
+    var newNode = buildItemRow();
+    newNode.selectedItem = {itemId: 0, multiplier: 1};
+    document.querySelectorAll('[additem]').forEach(node => {node.remove()});
+
+    selectedItems.push(newNode.selectedItem);
+    itemSelection.appendChild(newNode);
+  },
+  removeItem: function(button){
+    var itemDiv = button.closest('[itemselect]');
+    selectedItems.splice(selectedItems.indexOf(itemDiv.selectedItem), 1);
+
+    if (!itemDiv.nextElementSibling)
+      itemDiv.previousElementSibling.appendChild(buildAddItemButton());
+    itemDiv.remove()
+  },
+  logItems: function(){
+    console.log(generateSummary(selectedItems));
+  }
 }
 
