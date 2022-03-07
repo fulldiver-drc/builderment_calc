76019745a71ee1c4be75c6e84b0360f82a92a62b
fulldiver-drc <89520557+fulldiver-drc@users.noreply.github.com>
2022-03-07 09:37:52 +0800

    Update calculatorSelection.js

--- scripts/calculatorSelection.js
+++ scripts/calculatorSelection.js
@@ -1,16 +1,21 @@
-import {getItemList, sortSettings} from './itemManagement.js';
+import {getItemList, getItemDetails, sortSettings} from './itemManagement.js';
 
 var selectedItems = [];
 
 window.selectItem = function(select, isMultiplier=false){
-  var item = select.closest('[itemselect]').selectedItem;
+  var itemDiv = select.closest('[itemselect]');
+  var item = itemDiv.selectedItem;
   if (isMultiplier){
     if (select.valueAsNumber < 1)
        select.value = 1;
     item.multiplier = select.valueAsNumber;
   }
-  else
+  else{
     item.itemId = parseInt(select.value);
+    var preview = itemDiv.querySelector('.item-quick-rate')[0];
+    preview.setAttribute('itemid', item.itemId);
+    preview.innerHTML = `Rate: ${Math.round(getItemDetails(item.itemId).Base * item.multiplier)} / min`
+  }
 }
 
 function buildItemList(){
@@ -40,6 +45,7 @@ function buildItemRow(){
   nodeTemplate.innerHTML = `<select required onchange='selectItem(this)' class='item-select'>${optionsText}</select>`;
   nodeTemplate.innerHTML += `<input type='number' placeholder='Total level multiplier' value='1' class='item-multiply' onfocusout='selectItem(this, true)' min=1 />`;
   nodeTemplate.innerHTML += `<a onclick='removeItem(this)' class='ico' removeitem>&#xE108</a><a onclick='addItem()' class='ico' additem>&#xE109</a>`;
+  nodeTemplate.innerHTML += `<div itemid=0 mult=0 class='item-quick-rate'>Rate: N/A</div>`
   return nodeTemplate;
 }
 
