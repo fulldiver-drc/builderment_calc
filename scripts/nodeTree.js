4f70584a5e5a205ba8d88720988f2ce1739fcd1f
fulldiver-drc <89520557+fulldiver-drc@users.noreply.github.com>
2022-03-08 17:24:39 +0800

    Update nodeTree.js

--- scripts/nodeTree.js
+++ scripts/nodeTree.js
@@ -1,33 +1,35 @@
 var NodeTree = function(){
   var tree = this;
   this.createNode = function(objTree, parent, subTreeLabel, collapsedNodeBuilder, expandedNodeBuilder){
-    parent.innerHTML = '';
-    
-    var node = document.createElement('div');
-    node.setAttribute('class', 'tree-node');
-    
-    var label = document.createElement('div');
-    label.setAttribute('class', 'node-label-wrapper');
-    label.setAttribute('onclick', 'nodeTree.toggle(this)');
-    label.innerHTML = `<div class='node-marker'></div>`;
-    
-    var collapsed = collapsedNodeBuilder(objTree);
-    collapsed.classList.add('node-label collapsed');
+    objTree.forEach(obj => {
+      parent.innerHTML = '';
 
-    var expanded = expandedNodeBuilder(objTree);
-    expanded.classList.add('node-label expanded');
-    node.appendChild(label);
-    label.appendChild(collapsed);
-    label.appendChild(expanded);
+      var node = document.createElement('div');
+      node.setAttribute('class', 'tree-node');
 
-    var subTree = objTree[subTreeLabel];
-    if (subTree && subTree.length > 0){
-      var subNode = document.createElement('div');
-      subNode.setAttribute('class', 'subtree-wrapper');
-      subTree.forEach(x => {tree.createNode(x, subNode, subTreeLabel, collapsedNodeBuilder, expandedNodeBuilder)});
-      node.appendChild(subNode);
-    }
-    parent.appendChild(node);
+      var label = document.createElement('div');
+      label.setAttribute('class', 'node-label-wrapper');
+      label.setAttribute('onclick', 'nodeTree.toggle(this)');
+      label.innerHTML = `<div class='node-marker'></div>`;
+
+      var collapsed = collapsedNodeBuilder(objTree);
+      collapsed.classList.add('node-label collapsed');
+
+      var expanded = expandedNodeBuilder(objTree);
+      expanded.classList.add('node-label expanded');
+      node.appendChild(label);
+      label.appendChild(collapsed);
+      label.appendChild(expanded);
+
+      var subTree = objTree[subTreeLabel];
+      if (subTree && subTree.length > 0){
+        var subNode = document.createElement('div');
+        subNode.setAttribute('class', 'subtree-wrapper');
+        subTree.forEach(x => {tree.createNode(x, subNode, subTreeLabel, collapsedNodeBuilder, expandedNodeBuilder)});
+        node.appendChild(subNode);
+      }
+      parent.appendChild(node);
+    });
   }
   
   this.toggle = function(element){
