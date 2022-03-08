4f35f941fa3df075842af7e9e9dc67c11e0a7c4c
fulldiver-drc <89520557+fulldiver-drc@users.noreply.github.com>
2022-03-08 23:37:54 +0800

    Update nodeTree.js

--- scripts/nodeTree.js
+++ scripts/nodeTree.js
@@ -2,12 +2,16 @@ var NodeTree = function(){
   var tree = this;
   this.createNode = function(objTree, parent, subTreeLabel, labelBuilder, previewBuilder, preview, topLayer = true){
     objTree.forEach(obj => {
-      if (topLayer)
-        parent.innerHTML = '';
       
+      var subTree = obj[subTreeLabel];
       
       var node = document.createElement('div');
       node.setAttribute('class', 'tree-node');
+      
+      if (topLayer){
+        parent.innerHTML = '';
+        node.previewPand = preview;
+      }
 
       var labelDiv = document.createElement('div');
       labelDiv.setAttribute('class', 'node-label-wrapper');
@@ -15,20 +19,18 @@ var NodeTree = function(){
       var marker = document.createElement('div');
       marker.setAttribute('class', 'node-marker');
       marker.setAttribute('onclick', 'nodeTree.toggle()');
-      labelDiv.appendChild(marker);
       node.appendChild(labelDiv);
 
       var previewNode = previewBuilder(obj);
       var label = labelBuilder(obj);
       label.classList.add('node-label');
-      label.previewPane = preview;
       label.previewNode = previewNode;
       label.setAttribute('onclick', 'nodeTree.preview()');
       label.setAttribute('ondblclick', 'nodeTree.toggle()');
       labelDiv.appendChild(label);
 
-      var subTree = obj[subTreeLabel];
       if (subTree && subTree.length > 0){
+        labelDiv.prepend(marker);
         var subNode = document.createElement('div');
         subNode.setAttribute('class', 'subtree-wrapper');
         tree.createNode(subTree, subNode, subTreeLabel, labelBuilder, previewBuilder, preview, false);
@@ -39,11 +41,15 @@ var NodeTree = function(){
   }
   
   this.preview = function(){
-    var element = event.currentTarget;
     event.preventDefault();
-    element.previewPane.innerHTML = '';
-    element.previewPane.appendChild(element.previewNode);
-    element.closest('.tree-node:not(.tree-node .tree-node)').querySelectorAll('.node-label[selected]').forEach(x => {x.removeAttribute('selected');});
+    
+    var element = event.currentTarget;
+    
+    var rootNode = element.closest('.tree-node:not(.tree-node .tree-node)');
+    rootNode.previewPane.innerHTML = '';
+    rootNode.previewPane.appendChild(element.previewNode);
+    rootNode.querySelectorAll('.node-label[selected]').forEach(x => {x.removeAttribute('selected');});
+    
     element.setAttribute('selected', '');
   }
   
